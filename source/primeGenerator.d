245414b9c60e5b77ec7e00df5a4003704ad2a030
Seth Berman <bermanseth1@gmail.com>
2018-07-14 21:43:30 -0600

    Streamlined code

--- source/primeGenerator.d
+++ source/primeGenerator.d
@@ -2,16 +2,14 @@ import std.math;
 
 bool primeCheck (int num) { //Function for checking if the number is prime
     
-    float numF = cast(float)num;
-    
-    if (floor(numF/2) == (numF/2)) { //Check if the number is even, not strictly necessary with main implementation
+    if (num%2==0) { //Check if the number is even, not strictly necessary with main implementation
        
        return false;
     }
        
-    for (float i=3; i <= sqrt(numF); i = i + 2) { //Checks divisors 
+    for (float i=3; i <= sqrt(cast(float)num); i = i + 2) { //Checks divisors 
             
-        if (floor(numF/i) == (numF/i)) {
+        if (num%i == 0) {
                
              return false;
         }
